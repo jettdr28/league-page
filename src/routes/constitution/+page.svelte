a5c316963163116afd27fb8dd9a3e2663ac36ae2
Jett Rosenstein <69054374+jettdr28@users.noreply.github.com>
2024-07-22 20:43:48 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,4 +1,17 @@
-<!DOCTYPE html>
+<script>
+    import { dues } from '$lib/utils/helper';
+    let one, oneOne, oneOneOne, oneTwo, oneTwoOne, oneTwoTwo, oneTwoThree, oneTwoFour, oneTwoFive, oneThree;
+    let two, twoOne, twoTwo, twoThree;
+    let three, threeOne;
+    let four, fourOne, fourTwo;
+    let five, fiveOne;
+    let six, sixOne, sixTwo, sixThree;
+    let seven, sevenOne, sevenTwo, sevenThree;
+
+    const goToSection = (section) => {
+        const top = section.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({left: 0, top, behavior: 'smooth'});
+    }
 <html lang="en">
 <head>
     <meta charset="UTF-8">
